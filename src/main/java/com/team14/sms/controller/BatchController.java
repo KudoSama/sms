c22b41e5b3e8dbacd469b17f7db5f1234f603161
Kudo1226398122 <1226398122abc@gmail.com>
2022-06-27 11:22:47 +0800

    减少controller层逻辑，减轻前端项目开发压力

--- src/main/java/com/team14/sms/controller/BatchController.java
+++ src/main/java/com/team14/sms/controller/BatchController.java
@@ -39,55 +39,13 @@ public class BatchController extends BaseController {
     @ResponseBody
     @ApiOperation(value = "添加批次接口",notes = "应传入：batchId, batchDatestart（时间戳, batchDateend（时间戳")
     public JsonResponse addBatch(@RequestBody @Valid Batch batch) {
-        User loginUser = SessionUtils.getCurUser();
-        // System.out.println(loginUser);
-        // 只有学校用户才能添加批次
-        if (loginUser.getUserType().equals("4")) {
-            try {
-                // 未填写批次号、开始时间或结束时间
-                if (batch.getBatchId() == null || batch.getBatchDateend() == null ||
-                        batch.getBatchDatestart() == null) {
-                    return JsonResponse.failure("添加失败，您未填写批次号、开始日期或结束时间");
-                } else {
-                    // 结束时间小于开始时间
-                    if (batch.getBatchDateend().compareTo(batch.getBatchDatestart()) <= 0 ) {
-                        return JsonResponse.failure("添加失败，您填写的结束日期早于开始日期");
-                    } else {
-                        batchService.save(batch);
-                        return JsonResponse.successMessage("添加成功");
-                    }
-                }
-            } catch (Exception e) {
-                return JsonResponse.failure("添加失败");
-            }
-        }
-        return JsonResponse.failure("添加失败，您无本操作权限，请联系系统管理员!");
+        return batchService.addState(batch);
     }
 
     @RequestMapping(value = "/modify", produces = "application/json;charset=utf-8")
     @ResponseBody
     @ApiOperation(value = "修改批次接口",notes = "应传入：batchId, batchDatestart（时间戳, batchDateend（时间戳")
     public JsonResponse modifyBatch(@RequestBody @Valid Batch batch) {
-        User loginUser = SessionUtils.getCurUser();
-        System.out.println(loginUser);
-        // 只有学校用户才能修改批次
-        if (loginUser.getUserType().equals("school")) {
-            // 未填写开始时间或结束时间
-            if (batch.getBatchDateend() == null || batch.getBatchDatestart() == null) {
-                return JsonResponse.failure("添加失败，您未填写开始日期或结束时间");
-            } else {
-                // 结束时间小于开始时间
-                if (batch.getBatchDateend().compareTo(batch.getBatchDatestart()) <= 0) {
-                    return JsonResponse.failure("修改失败，您填写的结束日期早于开始日期");
-                } else {
-                    boolean temp = batchService.updateByBatch(batch);
-                    if (temp) {
-                        return JsonResponse.successMessage("修改成功");
-                    }
-                    return JsonResponse.successMessage("修改失败");
-                }
-            }
-        }
-        return JsonResponse.failure("添加失败，您无本操作权限，请联系系统管理员!");
+        return batchService.updateByBatch(batch);
     }
 }
--- src/main/java/com/team14/sms/service/impl/BatchServiceImpl.java
+++ src/main/java/com/team14/sms/service/impl/BatchServiceImpl.java
@@ -1,12 +1,13 @@
 package com.team14.sms.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.team14.sms.vo.Batch;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.team14.sms.base.JsonResponse;
 import com.team14.sms.mapper.BatchMapper;
 import com.team14.sms.service.BatchService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.team14.sms.vo.College;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.team14.sms.utls.SessionUtils;
+import com.team14.sms.vo.Batch;
+import com.team14.sms.vo.User;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,20 +21,60 @@ import org.springframework.stereotype.Service;
 @Service
 public class BatchServiceImpl extends ServiceImpl<BatchMapper, Batch> implements BatchService {
 
-    @Autowired
-    private BatchService batchService;
-
     @Override
-    public boolean updateByBatch(Batch batch) {
+    public JsonResponse updateByBatch(Batch batch) {
         QueryWrapper<Batch> wrapper =new QueryWrapper<>();
         wrapper.eq("batch_id", batch.getBatchId());
-        Batch temp = super.getOne(wrapper);
-        if (temp == null) {
-            return false;
+        User loginUser = SessionUtils.getCurUser();
+        // System.out.println(loginUser);
+        // 只有学校用户才能修改批次
+        if (loginUser.getUserType().equals("1")) {
+            // 未填写开始时间或结束时间
+            if (batch.getBatchDateend() == null || batch.getBatchDatestart() == null) {
+                return JsonResponse.failure("修改失败，您未填写开始日期或结束时间");
+            } else {
+                // 结束时间小于开始时间
+                if (batch.getBatchDateend().compareTo(batch.getBatchDatestart()) <= 0) {
+                    return JsonResponse.failure("修改失败，您填写的结束日期早于开始日期");
+                } else {
+                    Batch temp = super.getOne(wrapper);
+                    if (temp == null) {
+                        return JsonResponse.failure("修改失败，不存在该批次");
+                    }
+                    temp.setBatchDatestart(batch.getBatchDatestart());
+                    temp.setBatchDateend(batch.getBatchDateend());
+                    super.update(temp, wrapper);
+                    return JsonResponse.successMessage("修改成功");
+                }
+            }
         }
-        temp.setBatchDatestart(batch.getBatchDatestart());
-        temp.setBatchDateend(batch.getBatchDateend());
-        batchService.update(temp, wrapper);
-        return true;
+        return JsonResponse.failure("修改失败，您无本操作权限，请联系系统管理员!");
+    }
+
+    @Override
+    public JsonResponse addState(Batch batch) {
+        User loginUser = SessionUtils.getCurUser();
+        // System.out.println(loginUser);
+        // 只有学校用户才能添加批次
+        if (loginUser.getUserType().equals("1")) {
+            try {
+                // 未填写批次号、开始时间或结束时间
+                if (batch.getBatchId() == null || batch.getBatchDateend() == null ||
+                        batch.getBatchDatestart() == null) {
+                    return JsonResponse.failure("添加失败，您未填写批次号、开始日期或结束时间");
+                } else {
+                    // 结束时间小于开始时间
+                    if (batch.getBatchDateend().compareTo(batch.getBatchDatestart()) <= 0 ) {
+                        return JsonResponse.failure("添加失败，您填写的结束日期早于开始日期");
+                    } else {
+                        super.save(batch);
+                        return JsonResponse.successMessage("添加成功");
+                    }
+                }
+            } catch (Exception e) {
+                return JsonResponse.failure("添加失败");
+            }
+        }
+        return JsonResponse.failure("添加失败，您无本操作权限，请联系系统管理员!");
     }
 }
