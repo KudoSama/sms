3a1bcbb5eb9282c5215da8f5be5210f898d3bdb8
Kudo1226398122 <1226398122abc@gmail.com>
2022-06-27 14:56:28 +0800

    小修改

---
+++ src/main/java/com/team14/sms/controller/StuApplyController.java
@@ -0,0 +1,27 @@
+package com.team14.sms.controller;
+
+
+import com.team14.sms.base.JsonResponse;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.team14.sms.base.BaseController;
+
+import java.util.Date;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author wmj
+ * @since 2022-06-27
+ */
+@Controller
+@RequestMapping("/api/stuApply")
+public class StuApplyController extends BaseController {
+
+}
---
+++ src/main/java/com/team14/sms/service/impl/StuApplyServiceImpl.java
@@ -0,0 +1,20 @@
+package com.team14.sms.service.impl;
+
+import com.team14.sms.vo.StuApply;
+import com.team14.sms.mapper.StuApplyMapper;
+import com.team14.sms.service.StuApplyService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wmj
+ * @since 2022-06-27
+ */
+@Service
+public class StuApplyServiceImpl extends ServiceImpl<StuApplyMapper, StuApply> implements StuApplyService {
+
+}
